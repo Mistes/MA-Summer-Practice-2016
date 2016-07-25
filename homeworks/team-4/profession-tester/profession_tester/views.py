3811ea0373ea00764d5220f881502863b14d1955
Molotoha <anton.lunyov@gmail.com>
2016-07-25 19:07:51 +0300

    fixed some DRY stuff, implemented request of next test

--- homeworks/team-4/profession-tester/profession_tester/views.py
+++ homeworks/team-4/profession-tester/profession_tester/views.py
@@ -1,32 +1,29 @@
-from flask import jsonify
+from flask import json, jsonify
 from profession_tester import app, models, db
 
-Tests = models.Tests
+Tests = models.Tests					
 
-example_tests = [
-	{
-		'id': 1,
-		'name': u'profession-test',
-		'type': 1,
-		'questions': [
-			{
-				'body': u'Question 1',
-				'answers': [
-					{
-						'body': u'Answer 1',
-						'profession': 1
-					},
-					{
-						'body': u'Answer 2',
-						'profession': 2
-					}
-				]
-			}
-		]
+def jsonify_test(my_test):
+	tmp_answers = []
+	tmp_questions = []
+	for q in my_test.questions:
+		for a in q.answers:
+			tmp_answers.append({
+				'body': a.answer_body,
+				'profession': a.profession_enum
+			})
+		tmp_questions.append({
+			'body': q.question_body,
+			'answers': tmp_answers
+		})
+		tmp_answers = []
+	return {
+		'id': my_test.id,
+		'name': my_test.name,
+		'type': my_test.type,
+		'questions': tmp_questions
 	}
-]
 					
-						
 
 @app.route('/')
 def hello():
@@ -40,48 +37,17 @@ def get_all_tests():
 	tmp_answers = []
 	tmp_questions = []
 	for t in tests:
-		for q in t.questions:
-			for a in q.answers:
-				tmp_answers.append({
-					'body': a.answer_body,
-					'profession': a.profession_enum
-				})
-			tmp_questions.append({
-				'body': q.question_body,
-				'answers': tmp_answers
-			})
-			tmp_answers = []
-		formatted_tests.append({
-			'id': t.id,
-			'name': t.name,
-			'type': t.type,
-			'questions': tmp_questions
-		})
-		tmp_questions = []
+		formatted_tests.append(jsonify_test(t))
 
-	return jsonify({'tests': formatted_tests}), 
-200, {'Content-Type': 'application/json'}
+	return jsonify({'tests': formatted_tests})
 
 @app.route('/tests/<int:id>', methods = ['GET'])
 def get_test(id):
-	test = Tests.query.get(id)
-	tmp_answers = []
-	tmp_questions = []
-	for q in test.questions:
-		for a in q.answers:
-			tmp_answers.append({
-				'body': a.answer_body,
-				'profession': a.profession_enum
-			})
-		tmp_questions.append({
-			'body': q.question_body,
-			'answers': tmp_answers
-		})
-		tmp_answers = []
-	return jsonify({
-		'id': test.id,
-		'name': test.name,
-		'type': test.type,
-		'questions': tmp_questions
-	})
+	test = Tests.query.get_or_404(id)
+	return jsonify(jsonify_test(test))
+	
 
+@app.route('/tests/request-test/<name>/<int:type>', methods = ['GET', 'POST'])
+def get_next_test(name, type):
+	test = Tests.query.filter_by(name = name, type = type).first()
+	return jsonify(jsonify_test(test))
